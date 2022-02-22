9768caebc886e58e9dcbeccc9791ce086503ad20
TA <appulegugure@gmail.com>
2022-02-22 18:11:59 +0900

    whileループの中修正

--- 05/if2_2.php
+++ 05/if2_2.php
@@ -6,12 +6,11 @@ $fund_raising = 101;
 echo "あなたの所持金は${poket_money}です" . '<br>';
 
 do {
-    if ($poket_money <= $fund_raising) {
-        echo 'あなたはこれ以上募金できません';
-        break;
-    }
+
     $poket_money = $poket_money - $fund_raising;
     echo "${fund_raising}円募金しました" . '<br>';
     echo "残り残高は${poket_money}です" . '<br>';
     
-} while (true);
+} while ($poket_money >= $fund_raising);
+
+echo 'あなたはこれ以上募金できません';
